d3c0468fd0169eb7447d10c188a2e88756f74f86
HenryDev <enrique.quiroz.89@gmail.com>
2021-01-01 08:29:37 -0600

    Renderizacion de los products, sin interfaz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Interface } from 'readline';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,52 @@ export class AppComponent {
 
 //array que usatra ngFor
 items = ['nicolas', 'javier', 'leonel']
+
+products  = [
+  {
+    id: '1',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 800,
+    description: 'Buena camisa'
+  },
+  {
+    id: '2',
+    image: 'assets/images/hoodie.png',
+    title: 'Hoodie',
+    price: 600,
+    description: 'No se que es un hoddie'
+  },
+  {
+    id: '3',
+    image: 'assets/images/mug.png',
+    title: 'Mug',
+    price: 700,
+    description: 'Para programar' 
+  },
+  {
+    id: '4',
+    image: 'assets/images/pin.png',
+    title: 'Pin',
+    price: 600,
+    description: 'EL titulo'
+  },
+  {
+    id: '5',
+    image: 'assets/images/stickers1.png',
+    title: 'Stickers',
+    price: 876  ,
+    description: 'Un mega Sticker'
+  },
+  {
+    id: '6',
+    image: 'assets/images/stickers2.png',
+    title: 'Stickers',
+    price: 800,
+    description: 'tro sticker'
+  },
+];
+
 
 //funcion al hacer click en  agregar Item
 agregarItem(){
