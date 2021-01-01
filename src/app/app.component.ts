4a3fcf5f61633201cfea943a4ba1ba1d9336332f
HenryDev <enrique.quiroz.89@gmail.com>
2021-01-01 15:56:31 -0600

    Es mejor usar Pipes que funciones en un componente, pipe tiene alto rendimiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,76 +5,73 @@ import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'web-store';
 
-//ngFor para representarlo
-items = ['nicolas', 'javier', 'leonel']
+  // ngFor para representarlo
+  items = ['nicolas', 'javier', 'leonel'];
 
-//ngFor tambien
-        /**Creamos una interfaza para poder tipar los objetos de products[]*/
-products : Product[] = [
-  {
-    id: '1',
-    image: 'assets/images/camiseta.png',
-    title: 'Camiseta',
-    price: 800,
-    description: 'Buena camisa'
-  },
-  {
-    id: '2',
-    image: 'assets/images/hoodie.png',
-    title: 'Hoodie',
-    price: 600,
-    description: 'No se que es un hoddie'
-  },
-  {
-    id: '3',
-    image: 'assets/images/mug.png',
-    title: 'Mug',
-    price: 700,
-    description: 'Para programar' 
-  },
-  {
-    id: '4',
-    image: 'assets/images/pin.png',
-    title: 'Pin',
-    price: 600,
-    description: 'EL titulo'
-  },
-  {
-    id: '5',
-    image: 'assets/images/stickers1.png',
-    title: 'Stickers',
-    price: 876  ,
-    description: 'Un mega Sticker'
-  },
-  {
-    id: '6',
-    image: 'assets/images/stickers2.png',
-    title: 'Stickers',
-    price: 800,
-    description: 'tro sticker'
-  },
-];
+  power  = 10;
+  // ngFor tambien
+  // Creamos una interfaza para poder tipar los objetos de products[]
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 800,
+      description: 'Buena camisa',
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 600,
+      description: 'No se que es un hoddie',
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 700,
+      description: 'Para programar',
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 600,
+      description: 'EL titulo',
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 876,
+      description: 'Un mega Sticker',
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 800,
+      description: 'tro sticker',
+    },
+  ];
 
-//funcion al hacer click en  agregar Item
-agregarItem(){
-    this.items.push('nuevo item')
+  // funcion al hacer click en  agregar Item
+  agregarItem(): void {
+    this.items.push('nuevo item');
+  }
+
+  eliminarItem(index: number): void {
+    this.items.splice(index, 1);
+  }
+
+  clickProduct(product: Product): void {
+    console.log('product' + product.title);
+    console.log('id' + product.id);
+  }
 }
-
-
-eliminarItem(index: number){
-  this.items.splice(index, 1);
-}
-
-
-clickProduct(product: Product){
-  console.log('product' + product.title);
-  console.log('id' + product.id );
-}
-
-}
-
--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -13,43 +13,45 @@ import {
 
 import { Product } from './../product.model';
 
-/**Ever last class */
+ // Ever last class
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements DoCheck, OnInit, OnDestroy {
-  /**Mediante Input le ingreso el objeto
-   * que recibi en app.component.html*/
+  // Mediante Input le ingreso el objeto
+  // que recibi en app.component.html
   @Input() product: Product; // En invocado en el template
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+  // Objeto tipo Fecha
+  today = new Date();
 
   constructor() {
     console.log('1.constructor'); // Me deja ver cuantas veces se crearo objetos
   }
 
-  /**Forma nativa de identificar cambios*/
-  //   ngOnChanges(changes: SimpleChanges) {
-  //     console.log('2.ngOnChanges'); // Seguimiento de cambios
-  //     console.log(changes);
+  // Forma nativa de identificar cambios
+  // ngOnChanges(changes: SimpleChanges) {
+  // console.log('2.ngOnChanges'); // Seguimiento de cambios
+  // console.log(changes);
   //   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     console.log('3.ngOnInit');
   }
 
-  /**Forma personlizada de identificar cambios*/
+  // Forma personlizada de identificar cambios
   ngDoCheck(): void {
     console.log('4.DoCheck');
   }
 
   ngOnDestroy(): void {
-    /**Util para quitar suscriptores y liberar memoria*/
+    // Util para quitar suscriptores y liberar memoria
     console.log('5.DoCheck');
   }
 
-  addCart() {
+  addCart(): void {
     console.log('3.Añadido al carrito');
     this.productClicked.emit(this.product.id);
   }
