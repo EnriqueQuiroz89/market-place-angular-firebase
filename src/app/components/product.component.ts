d36dca533e4bac915e7a31cb33488493b71580ab
HenryDev <enrique.quiroz.89@gmail.com>
2021-01-01 14:04:48 -0600

    ngOnChanges, ngOnInit, ngOnDestroy, ngOnChanges son parte de del ciclo de vida del componete y puede ser explotados de forma adecuada para otimizar el mismo

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,27 +1,52 @@
 // decoardor le da un contexto  ----> Le asigna un rol
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  OnInit,
+  DoCheck,
+  OnDestroy,
+} from '@angular/core';
 
-
-import { Product } from './../product.model'
+import { Product } from './../product.model';
 
 /**Ever last class */
-@Component({ selector: 'app-product',
-             templateUrl: './product.component.html' })
-export class ProductComponent {
+@Component({ selector: 'app-product', templateUrl: './product.component.html' })
+export class ProductComponent implements DoCheck, OnInit, OnDestroy {
+  /**Mediante Input le ingreso el objeto
+   * que recibi en app.component.html*/
+  @Input() product: Product; // En invocado en el template
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
-    /**Mediante Input le ingreso el objeto
-     * que recibi en app.component.html*/
-    @Input() product : Product;    // En invocado en el template
-    @Output() productClicked: EventEmitter<any> = new EventEmitter();
-    addCart(){
-        console.log('Añadido al carrito')
-        this.productClicked.emit(this.product.id)
-    }
+  constructor() {
+    console.log('1.constructor'); // Me deja ver cuantas veces se crearo objetos
+  }
 
-    
+  /**Forma nativa de identificar cambios*/
+  //   ngOnChanges(changes: SimpleChanges) {
+  //     console.log('2.ngOnChanges'); // Seguimiento de cambios
+  //     console.log(changes);
+  //   }
 
+  ngOnInit() {
+    console.log('3.ngOnInit');
+  }
 
+  /**Forma personlizada de identificar cambios*/
+  ngDoCheck(): void {
+    console.log('4.DoCheck');
+  }
 
+  ngOnDestroy(): void {
+    /**Util para quitar suscriptores y liberar memoria*/
+    console.log('5.DoCheck');
+  }
 
-
+  addCart() {
+    console.log('3.Añadido al carrito');
+    this.productClicked.emit(this.product.id);
+  }
 }
