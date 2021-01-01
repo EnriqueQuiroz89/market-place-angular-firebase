51a7ae390a2f555dd75464230eabd8c5437dbade
HenryDev <enrique.quiroz.89@gmail.com>
2021-01-01 08:11:01 -0600

    uso del (click)="function()" donde funcion() esta en el componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'web-store';
 
+//array que usatra ngFor
+items = ['nicolas', 'javier', 'leonel']
+
+//funcion al hacer click en  agregar Item
+agregarItem(){
+    this.items.push('nuevo item')
+}
+
+
+eliminarItem(index: number){
+  this.items.splice(index, 1);
+}
 
 }
 
