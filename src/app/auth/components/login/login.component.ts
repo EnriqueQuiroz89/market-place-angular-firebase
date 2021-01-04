ef26dce987abfdbadeecd1aa95c38569f3b04190
HenryDev <enrique.quiroz.89@gmail.com>
2021-01-04 12:18:50 -0600

    Se completan la funcion de login, logout y register- falta la proteccion del modulo Admin

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+//Impementar AuthService
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   form!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {
     this.buildForm();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login(event: Event) {
     event.preventDefault();
-    console.log(this.form.value);
+    if (this.form.valid) {
+      
+const email:string= this.form.value.email;
+const password:string= this.form.value.password;
+
+      this.authService
+        .login(email, password)
+        .then(() => {
+          this.router.navigate(['/admin']);
+        })
+        .catch(() => {
+          alert('no es valido');
+        });
+    }
   }
 
   private buildForm() {
@@ -32,5 +46,4 @@ export class LoginComponent implements OnInit {
       password: ['', [Validators.required]],
     });
   }
-
 }
