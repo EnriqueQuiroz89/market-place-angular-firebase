4ecbc7e3f65bfd0bcdb854bfaeff09cd3957ff74
HenryDev <enrique.quiroz.89@gmail.com>
2021-01-04 13:09:18 -0600

    Se configuro el redireccionamiento si el usuario no tiene permitido entrar

--- src/app/core/guardians/admin.guard.ts
+++ src/app/core/guardians/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 // Usar un pipe para determinar que hacer con la respuesta del Observable de AuthService
@@ -12,21 +12,20 @@ import { FixedSizeVirtualScrollStrategy } from '@angular/cdk/scrolling';
 export class AdminGuard implements CanActivate {
 
 
-constructor(private authService: AuthService){}
+constructor(private authService: AuthService, private router: Router){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
  // tap intercepta un flujo de datos pero no lo modifica
   return this.authService.hasUser().pipe(
-    tap(user=> console.log(user?.email)), // intercepto el flujo para ver por que pasaba al admin
-    map(user => user=== null ? false: true));
+    map(user => user=== null ? false: true), //despues del pipe ya no existe el user
+    tap(hasUser=>{  
+      if(!hasUser){
+        this.router.navigate(['/auth/login'])
+      }
+    }))
 
-  }
-  
-
-
-
- 
+}
 
 }
